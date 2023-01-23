e245717f8056a762b5d069a3633a6a351eb6ed6d
esolar07 <esolar07@gmail.com>
2023-01-23 17:59:10 -0500

    Pushing react project

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
@@ -10,12 +10,18 @@ axios.defaults.baseURL = "http://localhost:3001";
 axios.defaults.headers.common['Authorization'] = 'BEARER TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-ReactDOM.render(
-    <Router>
-            <App />
-    </Router>,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
+
+root.render(
+  <React.StrictMode>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+  </React.StrictMode>
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
