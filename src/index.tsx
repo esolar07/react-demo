9a2ad2384782ed7894d6ca634c5e28bedc515258
esolar07 <esolar07@gmail.com>
2023-01-25 10:21:18 -0500

    Pushing react project

--- src/index.tsx
+++ src/index.tsx
@@ -15,11 +15,9 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
